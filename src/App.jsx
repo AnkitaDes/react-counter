e323cc176e01b783b2ba912af3e2594e331c7e1d
AnkitaDes <desai_ankita@outlook.com>
2024-03-13 08:22:41 -0400

    batch update counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 import { useState } from "react";
 
 function App() {
-  let [counter, setCounter] = useState(15);
+  let [counter, setCounter] = useState(5);
   //et counter = 15;
 
   const addValue = () => {
+    // if (counter >= 0 && counter < 20) {
+    //   counter = counter + 1;
+    //   setCounter(counter + 1);
+
+    //   console.log("Clicked:", counter);
+    // }
+    //counter = counter + 1;
+
+    // if (counter >= 0 && counter < 20) {
+    //   //counter = counter + 1;
+    //   setCounter(counter + 1);
+    //   setCounter(counter + 1);
+    //   setCounter(counter + 1);
+    //   setCounter(counter + 1);
+
+    //   console.log("Clicked:", counter);
+    // }
+    // counter increases with 1
+
+    // counter increases as a batch
+    counter = counter + 1;
     if (counter >= 0 && counter < 20) {
-      counter = counter + 1;
-      setCounter(counter);
+      setCounter((prevCounter) => prevCounter + 1);
+      setCounter((prevCounter) => prevCounter + 1);
+      setCounter((prevCounter) => prevCounter + 1);
+      setCounter((prevCounter) => prevCounter + 1);
+
       console.log("Clicked:", counter);
     }
   };
